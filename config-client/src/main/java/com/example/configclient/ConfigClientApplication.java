e1301f3524404f412d342eb277242929cd68cdd5
hui <774877338@qq.com>
2018-10-08 19:21:13 +0800

    初始导入

---
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
