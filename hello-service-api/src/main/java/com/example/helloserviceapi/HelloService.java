e018ca4f207c08704edf021d265a0b40ecad0e98
hui <774877338@qq.com>
2018-10-09 09:00:01 +0800

    初始导入

--- hello-service-api/src/main/java/com/example/helloserviceapi/HelloService.java
+++ hello-service-api/src/main/java/com/example/helloserviceapi/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.helloserviceapi;
 
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,5 +11,8 @@ public interface HelloService {
     String hello();
 
     @RequestMapping("/hello2")
-    User hello(@RequestParam("name") String name,Integer age);
+//    那么需要在请求参数前加上@RequestParam注解修饰，Controller里面可以不加该注解修饰。
+//    feign中你可以有多个@RequestParam，但只能有不超过一个@RequestBody。
+//    feign中绑定参数必须通过value属性来指明具体的参数名，不然会抛出IllegalStateExcepton异常
+    User hello(@RequestParam("name")String name,@RequestParam("age") Integer age);
 }
