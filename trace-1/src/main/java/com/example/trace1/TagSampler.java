445eaea29cddb1cc95c4c97772fc75b18cb46036
yanghui <77487733@qq.com>
2018-12-19 22:15:20 +0800

    配置客户端

---
+++ trace-1/src/main/java/com/example/trace1/TagSampler.java
@@ -0,0 +1,28 @@
+package com.example.trace1;
+
+import brave.sampler.Sampler;
+import org.springframework.cloud.sleuth.instrument.web.ClientSampler;
+import zipkin2.Span;
+
+/**
+ * PackageName:com.example.trace1
+ * ClassName: TagSampler
+ *
+ * @author zha.jiangjiang
+ * @Description:
+ * @Date: 2018/11/3 22:10
+ * @see
+ * @since JDK 1.8
+ */
+//public class TagSampler implements Sampler {
+//    private String tag;
+//
+//    public TagSampler(String tag){
+//        this.tag=tag;
+//    }
+//
+//    @Override
+//    public boolean isSampled(long traceId) {
+//        return false;
+//    }
+//}
